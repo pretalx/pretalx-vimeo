abd479e64cc92208578bb2440b09221bd0af028c
Tobias Kunze <r@rixx.de>
2021-12-15 01:17:00 +0100

    Move to proper model

--- pretalx_vimeo/recording.py
+++ pretalx_vimeo/recording.py
@@ -1,29 +1,8 @@
 from pretalx.agenda.recording import BaseRecordingProvider
 
 
-def is_vimeo_url(url):
-    return "vimeo.com/" in url  # TODO better validation
-
-
-def get_embed_url(url):
-    if "player.vimeo.com" in url:
-        return url
-    if not is_vimeo_url(url):
-        return
-
-    url = url[url.find("vimeo.com/") + len("vimeo.com/") :]
-    video_id = url.split("/")[0]
-    return f"https://player.vimeo.com/video/{video_id}"
-
-
 class VimeoProvider(BaseRecordingProvider):
     def get_recording(self, submission):
-        path = self.event.settings.get(f"vimeo_url_{submission.code}")
-        if not path:
-            return
-        url = get_embed_url(path)
-        if not url:
-            return
-        iframe = f'<div class="embed-responsive embed-responsive-16by9"><iframe src="{url}" frameborder="0" allowfullscreen></iframe></div>'
-        csp_header = "https://player.vimeo.com"
-        return {"iframe": iframe, "csp_header": csp_header}
+        vimeo = getattr(submission, "vimeo", None)
+        if vimeo:
+            return {"iframe": vimeo.iframe, "csp_header": "https://player.vimeo.com"}
