92a36c42c407520f27a2af582ad5039b88680fb7
Tobias Kunze <r@rixx.de>
2023-09-26 16:36:11 +0200

    Fix missing f-string

--- pretalx_vimeo/forms.py
+++ pretalx_vimeo/forms.py
@@ -47,7 +47,7 @@ class VimeoUrlForm(forms.Form):
 
     def save(self):
         for talk in self.talks:
-            video_id = self.cleaned_data.get("video_id_{talk.submission.code}")
+            video_id = self.cleaned_data.get(f"video_id_{talk.submission.code}")
             if video_id:
                 VimeoLink.objects.update_or_create(
                     submission=talk.submission, defaults={"video_id": video_id}
