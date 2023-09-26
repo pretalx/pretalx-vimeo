f835168accdeeaa3f0f12de100a3db90ef757c11
Tobias Kunze <r@rixx.de>
2023-09-26 14:25:31 +0200

    Release v2.1.0

--- pretalx_vimeo/apps.py
+++ pretalx_vimeo/apps.py
@@ -13,7 +13,7 @@ class PluginApp(AppConfig):
             "Embed Vimeo videos as session recordings, and retrieve them via an API."
         )
         visible = True
-        version = "2.0.6"
+        version = "2.1.0"
 
     def ready(self):
         from . import signals  # NOQA
