3fbbfcfa5c213ff772b15c4be3ac76677142d5c7
Tobias Kunze <r@rixx.de>
2024-02-16 15:26:23 +0100

    Use SLUG_REGEX

--- pretalx_vimeo/urls.py
+++ pretalx_vimeo/urls.py
@@ -1,21 +1,21 @@
 from django.urls import re_path
-from pretalx.event.models.event import SLUG_CHARS
+from pretalx.event.models.event import SLUG_REGEX
 
 from .views import VimeoSettings, api_list, api_single
 
 urlpatterns = [
     re_path(
-        rf"^orga/event/(?P<event>[{SLUG_CHARS}]+)/settings/p/vimeo/$",
+        rf"^orga/event/(?P<event>{SLUG_REGEX})/settings/p/vimeo/$",
         VimeoSettings.as_view(),
         name="settings",
     ),
     re_path(
-        rf"^api/events/(?P<event>[{SLUG_CHARS}]+)/p/vimeo/$",
+        rf"^api/events/(?P<event>{SLUG_REGEX})/p/vimeo/$",
         api_list,
         name="api_list",
     ),
     re_path(
-        rf"^api/events/(?P<event>[{SLUG_CHARS}]+)/submissions/(?P<code>[A-Z0-9]+)/p/vimeo/$",
+        rf"^api/events/(?P<event>{SLUG_REGEX})/submissions/(?P<code>[A-Z0-9]+)/p/vimeo/$",
         api_single,
         name="api_single",
     ),
